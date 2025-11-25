bfb7ef98d2fc2888f4eb3d21327b0573e7e13e77
gggloriammmartin-jpg <maksim.khlebnikov@example.com>
2025-11-25 19:54:09 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WaveSplitApp());
+}
+
+class WaveSplitApp extends StatelessWidget {
+  const WaveSplitApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'WaveSplit');
+  }
+}
